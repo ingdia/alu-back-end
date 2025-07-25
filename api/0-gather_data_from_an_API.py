96d27b743759770d4766827c214469a3a276db17
ingdia <d.ingabire2@alustudent.com>
2025-07-26 00:14:07 +0200

    removing the error

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,25 +7,22 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    emp_id = sys.argv[1]
+    employee_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com"
 
     # Get employee info
-    user_response = requests.get(f"{base_url}/users/{emp_id}")
-    user = user_response.json()
+    user = requests.get(f"{base_url}/users/{employee_id}").json()
     employee_name = user.get("name")
 
-    # Get employee's todos
-    todos_response = requests.get(f"{base_url}/todos", params={"userId": emp_id})
-    todos = todos_response.json()
+    # Get todos
+    todos = requests.get(f"{base_url}/todos", params={"userId": employee_id}).json()
 
     total_tasks = len(todos)
-    done_tasks = [task for task in todos if task.get("completed")]
+    done_tasks = [task for task in todos if task.get("completed") is True]
 
-    # Print header
+    # Output
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, len(done_tasks), total_tasks))
 
-    # Print completed tasks
     for task in done_tasks:
         print("\t {}".format(task.get("title")))
