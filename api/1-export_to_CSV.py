68383b7240eed9dce81a4a10d5b617bac9904132
ingdia <d.ingabire2@alustudent.com>
2025-07-25 06:50:57 +0200

    adding more

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exports employee TODO list to CSV"""
+"""Exports employee TODO list to CSV format"""
 
 import csv
 import requests
@@ -7,33 +7,29 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <employee_id>")
         sys.exit(1)
 
     employee_id = sys.argv[1]
 
-    # Get user info
+    # Fetch user info
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("User not found.")
-        sys.exit(1)
-    user_data = user_response.json()
-    username = user_data.get("username")
+    user = user_response.json()
+    username = user.get("username")
 
-    # Get user's tasks
+    # Fetch todos
     todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     todos_response = requests.get(todos_url)
-    tasks = todos_response.json()
+    todos = todos_response.json()
 
     # Write to CSV file
     file_name = f"{employee_id}.csv"
     with open(file_name, mode='w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in tasks:
+        for task in todos:
             writer.writerow([
                 employee_id,
                 username,
-                task.get("completed"),
+                str(task.get("completed")),
                 task.get("title")
             ])
