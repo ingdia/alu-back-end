3e26e5f5793b4abb13a7a67c9d5280526b599a2f
ingdia <d.ingabire2@alustudent.com>
2025-07-26 08:36:05 +0200

    correcting errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,25 +4,44 @@ Access employee TODO list progress from a REST API
 """
 
 import requests
-import sys
+from sys import argv
+""" modules to work with """ 
 
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
+def get_employee_todos_progress(employee_id):
+    """  a function to get employee info about Todo list"""
+    try:
+        url =   "https://jsonplaceholder.typicode.com"
+        # user_info = requests.get(f"{url}/users/{employee_id}")
+        user_datas = requests.get(url + f"/users/{employee_id}")
+        user_data = user_datas.json()
+        employee_name = user_data['name']
 
-    # Get employee info
-    user = requests.get(f"{base_url}/users/{employee_id}").json()
-    employee_name = user.get("name")
+        """fetching the todos of the employee"""
+        todos_list = requests.get(url +f"/todos?userId={employee_id}")
+        json_todos_list = todos_list.json()
 
-    # Get todos
-    todos = requests.get(f"{base_url}/todos", params={"userId": employee_id}).json()
+        total_tasks = len(json_todos_list)
+        task_done = [task for task in json_todos_list if task['completed']]  
+        no_task_done = len(task_done)
 
-    total_tasks = len(todos)
-    done_tasks = [task for task in todos if task.get("completed") is True]
+        """returning the employee info and tasks done"""
+        print(f"Employee {employee_name} is done with task("
+              f"{no_task_done}/{total_tasks}):")
+        
+        """title of completed tasks"""
+        for task in task_done:
+            print(f"\t {task['title']}")
 
-    # Output
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(done_tasks), total_tasks))
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    for task in done_tasks:
-        print("\t {}".format(task.get("title")))
+if __name__ == '__main__':
+    if len(argv) != 2:
+         print("Usage: Script <employee_id>")
+    else:
+        get_employee_todos_progress(argv[1]) 
+
+
+          
+
+  
